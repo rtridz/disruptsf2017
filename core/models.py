89aca474742e05e27d03f58ae7fa15aaed53a28e
Elena Andreeva <maerville@ya.ru>
2017-09-16 14:40:08 -0700

    Added goods and demands

--- core/models.py
+++ core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Affected(models.Model):
+
+class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
@@ -13,12 +14,34 @@ class Affected(models.Model):
     location_long = models.FloatField()
 
 
-class HelpTicket(models.Model):
-    user_created = models.ForeignKey(Affected, on_delete=models.CASCADE)
+class Affected(Person):
+    shelter_going = models.ForeignKey(Shelter)
+    shelter_signed_in = models.ForeignKey(Shelter)
+
+
+class AssistanceTicket(models.Model):
+    user_created = models.ForeignKey(Person, on_delete=models.CASCADE)
     date_created = models.DateField()
     type_of_assistance = models.TextField()
-    level_of_urgency = models.IntegerField()
+
     date_needed = models.DateField()
+
+    CRITICAL = 1
+    URGENT = 2
+    NORMAL = 3
+    DESIRABLE = 4
+    STATUS_CHOICES = (
+        (CRITICAL, 'Critical'),
+        (URGENT, 'Urgent'),
+        (NORMAL, 'Normal'),
+        (DESIRABLE, 'Desirable')
+    )
+    status = models.IntegerField(choices=STATUS_CHOICES, default=NORMAL)
+
+
+class AssistanceTicketReaction(models.Model):
+    ticket = models.ForeignKey(AssistanceTicket)
+    person_reacted = models.ForeignKey(Person)
 
 
 class HelpProvider(models.Model):
@@ -27,6 +50,29 @@ class HelpProvider(models.Model):
 
 class Shelter(models.Model):
     provider = models.ForeignKey(HelpProvider, on_delete=models.CASCADE)
+    shelter_name = models.CharField(max_length=50)
     location_lat = models.FloatField()
     location_long = models.FloatField()
-    
+    address = models.CharField(max_length=100)
+    max_capacity = models.IntegerField()
+    people_inside = models.IntegerField()
+    people_coming = models.IntegerField()
+
+
+class GoodsDemands(models.Model):
+    FOOD = 1
+    WATER = 2
+    BLANKET = 3
+    PAIN_KILLER = 4
+    SEDATIVE = 5
+    GOODS_TYPES = (
+        (FOOD, 'food pack'),
+        (WATER, 'water pack (2l)'),
+        (BLANKET, 'blanket'),
+        (PAIN_KILLER, 'pain killer'),
+        (SEDATIVE, "sedative")
+    )
+    good_type = models.IntegerField(choices=GOODS_TYPES)
+    qty = models.IntegerField()
+    who_demands = models.ForeignKey(Person)
+    shelter = models.ForeignKey(Shelter)
